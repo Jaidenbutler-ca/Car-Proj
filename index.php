329b728c3e235f0d81e9989d9a1c8c20f3bcf1ea
Jaiden Butler <jbutler28@nait.ca>
2023-10-05 13:27:37 -0600

    Got the random display working

--- index.php
+++ index.php
@@ -1,13 +1,37 @@
+<?php 
+    include ("test.php");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php
-    include ("test.php")?>
-    <h1>Test Html To PHP</h1>
+    <div ="Top_Of_Page">
+        <h1>Vehicle Display Test</h1>
+
+        <table>
+            <tr>
+                <th>Year</th>
+                <th>Make</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Dealer</th>
+                <th>City</th>
+            </tr>
+            <tr>
+                <td><?php echo $year; ?></td>
+                <td><?php echo $make; ?></td>
+                <td><?php echo $name; ?></td>
+                <td><?php echo $price; ?></td>
+                <td><?php echo $dealer; ?></td>
+                <td><?php echo $city; ?></td>
+            </tr>
+        </table>
+    </div>
 </body>
 </html>
--- test.php
+++ test.php
@@ -1,6 +1,7 @@
 <?php
     $user = "localhost";
     $pass = "aw";
+    $year = 0;
 
 
     $ODBCConnection = odbc_connect("DRIVER={ODBC Driver 18 for SQL Server};SERVER=JAIDENBUTLER-CA\SQLEXPRESS;Database=car", "Python", "Weemee12");
@@ -9,9 +10,18 @@
     $RecordSet = odbc_exec($ODBCConnection, $query);
 
         while (odbc_fetch_row($RecordSet)) {
-            $result = odbc_result($RecordSet, "year") . " " . odbc_result($RecordSet, "make") . " " . odbc_result($RecordSet, "name") . " " . odbc_result($RecordSet, "price") . " " . odbc_result($RecordSet, "dealer") . " " . odbc_result($RecordSet, "city");
-        }
+           # $result = odbc_result($RecordSet, "year") . " " . odbc_result($RecordSet, "make") . " " . odbc_result($RecordSet, "name") . " " . odbc_result($RecordSet, "price") . " " . odbc_result($RecordSet, "dealer") . " " . odbc_result($RecordSet, "city");
 
-        print($result);
+            $year = odbc_result($RecordSet, "year");
+            $make = odbc_result($RecordSet, "make");
+            $name = odbc_result($RecordSet, "name");
+            $price = odbc_result($RecordSet, "price");
+            $dealer = odbc_result($RecordSet, "dealer");
+            $city = odbc_result($RecordSet, "city");
+        }
+        
+
+
+
         odbc_close($ODBCConnection);
     ?>
