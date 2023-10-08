98626e6219dd489a9be02dde2ecbdbbef38f36b9
Jaiden Butler <jbutler28@nait.ca>
2023-10-08 17:41:30 -0600

    half ass solution

--- test.php
+++ test.php
@@ -1,21 +1,40 @@
 <?php
 // session_start();
+$modelVariable = "%";
+$makeVariable = "%";
+$yearMinVariable = 0;
+$yearMaxVariable = 2025;
+$minPrice = '%';
+$maxPrice = '';
+
+
+
+
 
 if (isset($_SESSION["makeVariable"])) {
     $makeVariable = $_SESSION["makeVariable"];
 } else {
     $makeVariable = "";
 }
+if (isset($_SESSION["modelVariable"])) {
+    $modelVariable = $_SESSION["modelVariable"];
+} else {
+    $modelVariable = "";
+}
 
+if(isset($_SESSION["yearMinVariable"])) {
+    $yearMinVariable = $_SESSION["yearMinVariable"];
+} else {
+    $yearMinVariable = 0;
+}
 
-$user = "localhost";
-$pass = "aw";
-$year = 0;
+if (isset($_SESSION["yearMaxVariable"])) {
+    $yearMaxVariable = $_SESSION["yearMaxVariable"];
+} else {
+    $yearMaxVariable = 9999;
+}
 
-// $makeVariable = '%';
 $yearVariable = '%';
-$modelVariable = '%';
-
 
 $minePriceVariable = 0;
 $maxPriceVariable = 999999999; # 9 9's
@@ -25,10 +44,10 @@ $query = strtoupper("SELECT year, make, name, price, dealer, city FROM info ORDE
 
 # Testing  here
 
-$query2 = "SELECT year, make, name, price, dealer, city FROM info WHERE make LIKE ? AND name LIKE ? AND year LIKE ? ORDER BY NEWID()";
+$query2 = "SELECT year, make, name, price, dealer, city FROM info WHERE make LIKE ? AND name LIKE ? AND year LIKE ? and year > ? and year < ? ORDER BY NEWID()";
 $stmt = odbc_prepare($ODBCConnection, $query2);
 
-odbc_execute($stmt, [$makeVariable, $modelVariable, $yearVariable]);
+odbc_execute($stmt, [$makeVariable, $modelVariable, $yearVariable, $yearMinVariable, $yearMaxVariable]);
 
 while ($row = odbc_fetch_array($stmt)) {
     $year = $row['year'];
@@ -41,9 +60,10 @@ while ($row = odbc_fetch_array($stmt)) {
 
 echo "Make variable test.php is: " . $makeVariable;
 echo "  Model variable test.php is: " . $modelVariable;
+echo "  Year variable test.php is: " . $yearMinVariable;
+echo " Max year variable test.php: " . $yearMaxVariable;
 
 # End Testing
 
 odbc_close($ODBCConnection);
 ?>
-i
