271c439d513fed9a9469625460a683f54bb9e5ca
Jaiden Butler <jbutler28@nait.ca>
2023-10-08 12:04:54 -0600

    Figured out make search function

--- index.php
+++ index.php
@@ -1,108 +1,117 @@
 <?php 
-    include ("test.php");
+    
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
-    <script src="myscripts.js"></script>
-</head>
-<body>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Penis</title>
+        <link rel="stylesheet" href="style.css" />
+        <script src="myscripts.js"></script>
+    </head>
+    <body>
+        <?php
+        session_start();
 
-<?php
+
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $makeVariable = $_POST["selectMake"];
    
             // Save $makeVariable to a session variable
             $_SESSION["makeVariable"] = $makeVariable;
-            header("Location: index.php?make= " . urlencode($makeVariable));
+
+            header("Location: index.php");
+            exit();
         }
-        session_start();
-        if (isset($GET["make"])) {
-            $makeVariable = $_GET["make"];
+        if (isset($_SESSION["makeVariable"])) {
+            $makeVariable = $_SESSION["makeVariable"];
+        } else {
+            $makeVariable = "";
         }
+
+        
+        include ("test.php");
+
     ?>
-    <div class="Top_Of_Page">
-        <h1>Vehicle Display Test</h1>
-        <img id="Placeholder_Image"src="placeholder.jpg" style="width:500px;height:500px;" alt="Placeholder Image">
 
-        <table class="Top_Of_Page">
-            <tr>
-                <th>Year</th>
-                <th>Make</th>
-                <th>Name</th>
-                <th>Price</th>
-                <th>Dealer</th>
-                <th>City</th>
-            </tr>
-            <tr>
-                <td><?php echo $year; ?></td>
-                <td><?php echo $make; ?></td>
-                <td><?php echo $name; ?></td>
-                <td><?php echo $price; ?></td>
-                <td><?php echo $dealer; ?></td>
-                <td><?php echo $city; ?></td>
-            </tr>
-        </table>
-    </div>
+        <div class="Top_Of_Page">
+            <h1>Vehicle Display Test</h1>
+            <img id="Placeholder_Image" src="placeholder.jpg" style="width: 500px; height: 500px;" alt="Placeholder Image" />
 
-    <!-- making a variable to hold the make in order to use it for search -->
+            <table class="Top_Of_Page">
+                <tr>
+                    <th>Year</th>
+                    <th>Make</th>
+                    <th>Name</th>
+                    <th>Price</th>
+                    <th>Dealer</th>
+                    <th>City</th>
+                </tr>
+                <tr>
+                    <td><?php echo $year; ?></td>
+                    <td><?php echo $make; ?></td>
+                    <td><?php echo $name; ?></td>
+                    <td><?php echo $price; ?></td>
+                    <td><?php echo $dealer; ?></td>
+                    <td><?php echo $city; ?></td>
+                </tr>
+            </table>
+        </div>
+
+        <!-- making a variable to hold the make in order to use it for search -->
+
+        <!-- Testing Here -->
+
+        <!-- Changed this -->
+        <form method="POST">
+            <!-- hopefully it works, do it when you get home -->
+
+            <label for="selectMake">Make:</label>
+            <select name="selectMake" id="selectMake">
+                <option value="%" selected>Any</option>
+                <option value="Acura">Acura</option>
+                <option value="BMW">BMW</option>
+                <option value="Chevrolet">Chevrolet</option>
+                <option value="Dodge">Dodge</option>
+                <option value="Ford">Ford</option>
+                <option value="GMC">GMC</option>
+                <option value="Honda">Honda</option>
+                <option value="Hyundai">Hyundai</option>
+                <option value="Infiniti">Infiniti</option>
+                <option value="Jeep">Jeep</option>
+                <option value="Kia">Kia</option>
+                <option value="Mercedes-Benz">Mercedes-Benz</option>
+                <option value="Nissan">Nissan</option>
+                <option value="Porsche">Porsche</option>
+                <option value="RAM">RAM</option>
+                <option value="Toyota">Toyota</option>
+            </select>
 
 
-    <!-- Testing Here -->
 
-<!-- Changed this -->
-<form method="POST">
-<!-- hopefully it works, do it when you get home -->
-    
-    
-<label for="selectMake">Make:</label>
-<select name="selectMake" id="selectMake">
-    <option value="Any" 
-    selected>Any</option>
-    <option value="Acura">Acura</option>
-    <option value="BMW">BMW</option>
-    <option value="Chevrolet">Chevrolet</option>
-    <option value="Dodge">Dodge</option>
-    <option value="Ford">Ford</option>
-    <option value="GMC">GMC</option>
-    <option value="Honda">Honda</option>
-    <option value="Hyundai">Hyundai</option>
-    <option value="Infiniti">Infiniti</option>
-    <option value="Jeep">Jeep</option>
-    <option value="Kia">Kia</option>
-    <option value="Mercedes-Benz">Mercedes-Benz</option>
-    <option value="Nissan">Nissan</option>
-    <option value="Porsche">Porsche</option>
-    <option value="RAM">RAM</option>
-    <option value="Toyota">Toyota</option>
-</select>
-<button type="submit">Submit</button>
-    </form>
 
-<label for="inputYear">Year:</label>
-<input type="text" id="inputYear" name="inputYear" placeholder="Year">
-    
-    
-<!-- working here -->
-<label for"dateSelect">Year:</label>
 
-    
-    
-    
 
-<label for="inputModel">Model:</label>
-<input type="text" id="inputModel" name="inputModel" placeholder="Model">
+        <label for="inputModel">Model:</label>
+        <input type="text" id="inputModel" name="selectModel" placeholder="Model" />
 
-<label for="MinPrice">Min Price:</label>
-<input type="text" id="MinPrice" name="MinPrice" placeholder="Min Price">
 
-<label for="MaxPrice">Max Price:</label>
-<input type="text" id="MaxPrice" name="MaxPrice" placeholder="Max Price">
 
-</body>
+
+
+        <label for="MinPrice">Min Price:</label>
+        <input type="text" id="MinPrice" name="MinPrice" placeholder="Min Price" />
+
+        <label for="MaxPrice">Max Price:</label>
+        <input type="text" id="MaxPrice" name="MaxPrice" placeholder="Max Price" />
+
+
+        <!-- dont fucking touch this -->
+        <button type="submit">Submit</button>
+
+
+        </form>
+    </body>
 </html>
--- test.php
+++ test.php
@@ -1,50 +1,49 @@
 <?php
-    $user = "localhost";
-    $pass = "aw";
-    $year = 0;
+// session_start();
 
-    $makeVariable = '%';
-    $modelVariable = '%';
-    $yearVariable =  '%';
-    $minePriceVariable = 0;
-    $maxPriceVariable = 999999999; # 9 9's
+if (isset($_SESSION["makeVariable"])) {
+    $makeVariable = $_SESSION["makeVariable"];
+} else {
+    $makeVariable = "";
+}
 
 
-    $ODBCConnection = odbc_connect("DRIVER={ODBC Driver 18 for SQL Server};SERVER=DESKTOP-H10UUI9\SQLEXPRESS;Database=car", "gang", "091516");
+$user = "localhost";
+$pass = "aw";
+$year = 0;
 
-    $query = strtoupper("SELECT year, make, name, price, dealer, city FROM info ORDER BY NEWID()");
+// $makeVariable = '%';
+$yearVariable = '%';
+$modelVariable = '%';
+
+
+$minePriceVariable = 0;
+$maxPriceVariable = 999999999; # 9 9's
+$ODBCConnection = odbc_connect("DRIVER={ODBC Driver 18 for SQL Server};SERVER=JAIDENBUTLER-CA\SQLEXPRESS;Database=car", "Python", "Weemee12");
+
+$query = strtoupper("SELECT year, make, name, price, dealer, city FROM info ORDER BY NEWID()");
 
 # Testing  here
-    
-    $query2 = "SELECT year, make, name, price, dealer, city FROM info WHERE make LIKE ? AND name LIKE ? AND year LIKE ? ORDER BY NEWID()";
-    $stmt = odbc_prepare($ODBCConnection, $query2);
 
-    odbc_execute($stmt, array($makeVariable, $modelVariable, $yearVariable));
+$query2 = "SELECT year, make, name, price, dealer, city FROM info WHERE make LIKE ? AND name LIKE ? AND year LIKE ? ORDER BY NEWID()";
+$stmt = odbc_prepare($ODBCConnection, $query2);
 
-    while ($row = odbc_fetch_array($stmt)) {
-        $year = $row['year'];
-        $make = $row['make'];
-        $name = $row['name'];
-        $price = $row['price'];
-        $dealer = $row['dealer'];
-        $city = $row['city'];
-    }
+odbc_execute($stmt, [$makeVariable, $modelVariable, $yearVariable]);
 
+while ($row = odbc_fetch_array($stmt)) {
+    $year = $row['year'];
+    $make = $row['make'];
+    $name = $row['name'];
+    $price = $row['price'];
+    $dealer = $row['dealer'];
+    $city = $row['city'];
+}
 
-
-
-
-
-
-
-
-
-
+echo "Make variable test.php is: " . $makeVariable;
+echo "  Model variable test.php is: " . $modelVariable;
 
 # End Testing
 
-
-
-
-        odbc_close($ODBCConnection);
-    ?>
+odbc_close($ODBCConnection);
+?>
+i
